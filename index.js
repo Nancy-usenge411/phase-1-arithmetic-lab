6ff80249020418c9ab77089bd30d8b5292df66e4
Nancy Usenge <nancyusenge411@gmail.com>
2024-07-02 11:32:59 +0300

    commit arithmetic labs

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+//Declare variable num1 and num2
+let num1=31;
+let num2=2;
 
-// Write your code here
+let multiply=num1*num2;
+const random= Math.floor(Math.random()*100)+1
+
+
+
+
+console.log('generates a random integer greater than 0');//true
+
+
+// Define variables num3 and num4
+let num3 = 14;
+let num4 = 10;
+
+let mod = num3 % num4;
+
+console.log('mod a remainder that calculates a remainder that is equal to 4'); // true,
+
+
+// Define  set of numbers
+let numbers = [10, 5, 20, 15, 18]
+
+const max =Math.max(10, 5, 20, 15,18)
+
+console.log('max will return 20 as the highest number in the set:'); //20 true
+
